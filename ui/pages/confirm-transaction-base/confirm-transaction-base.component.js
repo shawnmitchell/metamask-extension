e9ecfb146df7c2705243124279993159dbac15a0
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2021-10-07 23:10:59 +0100

    When the More details button of transaction error is clicked, show a dialog.
    TODO: Design the dialog.
    
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- ui/pages/confirm-transaction-base/confirm-transaction-base.component.js
+++ ui/pages/confirm-transaction-base/confirm-transaction-base.component.js
@@ -139,6 +139,7 @@ export default class ConfirmTransactionBase extends Component {
     isFailedTransaction: PropTypes.bool,
     removeTxFromFailedTxesToDisplay: PropTypes.bool,
     addTxToDisplay: PropTypes.bool,
+    showTransactionErrorDialog: PropTypes.func,
   };
 
   state = {
@@ -821,6 +822,11 @@ export default class ConfirmTransactionBase extends Component {
     }
   }
 
+  onErrorMessageClick(message) {
+    const { showTransactionErrorDialog } = this.props;
+    showTransactionErrorDialog(message);
+  }
+
   getNavigateTxData() {
     const { currentNetworkUnapprovedTxs, txData: { id } = {} } = this.props;
     const enumUnapprovedTxs = Object.keys(currentNetworkUnapprovedTxs);
@@ -1023,6 +1029,7 @@ export default class ConfirmTransactionBase extends Component {
         handleCloseEditGas={() => this.handleCloseEditGas()}
         currentTransaction={txData}
         isFailedTransaction={isFailedTransaction}
+        onErrorMessageClick={(message) => this.onErrorMessageClick(message)}
       />
     );
   }
