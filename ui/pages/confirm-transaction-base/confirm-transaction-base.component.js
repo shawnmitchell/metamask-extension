30084a2c998250dc03902094b69d4094f5360b23
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2021-10-20 04:44:35 +0100

    Convert Modal to Popover.
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- ui/pages/confirm-transaction-base/confirm-transaction-base.component.js
+++ ui/pages/confirm-transaction-base/confirm-transaction-base.component.js
@@ -823,11 +823,6 @@ export default class ConfirmTransactionBase extends Component {
     }
   }
 
-  onErrorMessageClick(message) {
-    const { showTransactionErrorDialog } = this.props;
-    showTransactionErrorDialog(message);
-  }
-
   getNavigateTxData() {
     const { currentNetworkUnapprovedTxs, txData: { id } = {} } = this.props;
     const enumUnapprovedTxs = Object.keys(currentNetworkUnapprovedTxs);
@@ -1030,7 +1025,6 @@ export default class ConfirmTransactionBase extends Component {
         handleCloseEditGas={() => this.handleCloseEditGas()}
         currentTransaction={txData}
         isFailedTransaction={isFailedTransaction}
-        onErrorMessageClick={(message) => this.onErrorMessageClick(message)}
       />
     );
   }
