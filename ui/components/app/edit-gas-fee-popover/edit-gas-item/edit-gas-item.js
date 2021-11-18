ecf132cd79013b30ea8340a264235790230d59c5
Alex <adonesky@gmail.com>
2021-11-18 15:06:47 -0600

    add tooltips

--- ui/components/app/edit-gas-fee-popover/edit-gas-item/edit-gas-item.js
+++ ui/components/app/edit-gas-fee-popover/edit-gas-item/edit-gas-item.js
@@ -21,6 +21,11 @@ import InfoTooltip from '../../../ui/info-tooltip';
 import UserPreferencedCurrencyDisplay from '../../user-preferenced-currency-display';
 
 import { useCustomTimeEstimate } from './useCustomTimeEstimate';
+import Typography from '../../../ui/typography';
+import {
+  COLORS,
+  FONT_WEIGHT,
+} from '../../../../helpers/constants/design-system';
 
 const EditGasItem = ({ priorityLevel, onClose }) => {
   const {
@@ -40,6 +45,8 @@ const EditGasItem = ({ priorityLevel, onClose }) => {
 
   if (gasFeeEstimates[priorityLevel]) {
     maxFeePerGas = gasFeeEstimates[priorityLevel].suggestedMaxFeePerGas;
+    maxPriorityFeePerGas =
+      gasFeeEstimates[priorityLevel].suggestedMaxPriorityFeePerGas;
   } else if (
     priorityLevel === PRIORITY_LEVELS.DAPP_SUGGESTED &&
     dappSuggestedGasFees
@@ -136,7 +143,86 @@ const EditGasItem = ({ priorityLevel, onClose }) => {
         )}
       </span>
       <span className="edit-gas-item__tooltip">
-        <InfoTooltip position="top" />
+        <InfoTooltip
+          wide
+          contentText={
+            <div className="edit-gas-item__tooltip__container">
+              <img
+                alt=""
+                width={110}
+                src={`./images/curve-${priorityLevel}.svg`}
+              />
+              <div className="edit-gas-item__tooltip__container__message">
+                <Typography fontSize="12px">
+                  {t(`${priorityLevel}GasSettingToolTipMessage`, [
+                    <span>
+                      <b>{t(priorityLevel)}</b>
+                    </span>,
+                  ])}
+                </Typography>
+              </div>
+              {priorityLevel !== PRIORITY_LEVELS.CUSTOM ? (
+                <div className="edit-gas-item__tooltip__container__values">
+                  <div>
+                    <Typography
+                      fontSize="12px"
+                      fontWeight={FONT_WEIGHT.BOLD}
+                      margin={0}
+                      className="edit-gas-item__tooltip__container__label"
+                    >
+                      {t('maxBaseFee')}
+                    </Typography>
+                    <Typography
+                      fontSize="12px"
+                      color={COLORS.NEUTRAL_GREY}
+                      margin={0}
+                      className="edit-gas-item__tooltip__container__value"
+                    >
+                      {maxFeePerGas}
+                    </Typography>
+                  </div>
+                  <div>
+                    <Typography
+                      fontSize="12px"
+                      fontWeight={FONT_WEIGHT.BOLD}
+                      margin={0}
+                      className="edit-gas-item__tooltip__container__label"
+                    >
+                      {t('priorityFee')}
+                    </Typography>
+                    <Typography
+                      fontSize="12px"
+                      color={COLORS.NEUTRAL_GREY}
+                      margin={0}
+                      className="edit-gas-item__tooltip__container__value"
+                    >
+                      {maxPriorityFeePerGas ?? maxPriorityFeePerGasValue}
+                    </Typography>
+                  </div>
+                  <div>
+                    <Typography
+                      fontSize="12px"
+                      fontWeight={FONT_WEIGHT.BOLD}
+                      margin={0}
+                      className="edit-gas-item__tooltip__container__label"
+                    >
+                      {t('gasLimit')}
+                    </Typography>
+                    <Typography
+                      fontSize="12px"
+                      color={COLORS.NEUTRAL_GREY}
+                      margin={0}
+                      className="edit-gas-item__tooltip__container__value"
+                    >
+                      {gasLimit}
+                    </Typography>
+                  </div>
+                </div>
+              ) : null}
+            </div>
+          }
+          position="top"
+        />
       </span>
     </div>
   );
